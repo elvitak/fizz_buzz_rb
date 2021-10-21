6c1bff520f7e1b8ed86d8651bbe3fdbc1666719d
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-10-21 10:51:42 +0200

    first test for returning a number only is green

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,3 +1,5 @@
+require_relative "../src/fizz_buzz"
+
 describe FizzBuzz do
 
     subject { FizzBuzz.new }
@@ -10,11 +12,12 @@ describe FizzBuzz do
         expect(subject.check(3)).to eq "fizz"
     end
 
-    it "is expected to return 'buzz' if the number is divisable by 5" do
+    xit "is expected to return 'buzz' if the number is divisable by 5" do
         expect(subject.check(5)).to eq "buzz"
     end
 
-    it "is expected to return 'fizz-buzz' if the number is divisable by 15" do
+    xit "is expected to return 'fizz-buzz' if the number is divisable by 15" do
         expect(subject.check(15)).to eq "fizz-buzz"
     end
+
 end
